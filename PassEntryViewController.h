a14d185e865386956308756954cbe47a6b4a1de5
David Beitey <david@davidjb.com>
2015-12-19 22:11:50 +1000

    s/rephorm/davidjb/g

--- PassEntryViewController.h
+++ PassEntryViewController.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012  Brian A. Mattern <rephorm@rephorm.com>.
+ * Copyright (C) 2015  David Beitey <david@davidjb.com>.
  * All Rights Reserved.
  * This file is licensed under the GPLv2+.
  * Please see COPYING for more information
